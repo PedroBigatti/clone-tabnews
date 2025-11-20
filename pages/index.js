7b932ec8f4b0319f0d61243395b7aa9a45491248
Pedro Bigatti <90102105+PedroBigatti@users.noreply.github.com>
2025-11-20 14:59:18 +0000

    Subiendo archivos iniciales del proyecto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <><h1>Nunca desacredite em ti!</h1><h2>O mundo poderá te jogar abaixo, mas quem te guarda não dorme.</h2></>
+    return <><h1>Nunca desacredite de ti!</h1><h2>O mundo poderá te jogar abaixo, mas quem te guarda não dorme.</h2></>
 }
 
 export default Home;
